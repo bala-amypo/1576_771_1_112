a7b12dd550a78a1e04f27c60a734cb5af8d4a2fb
bala-amypo <bala@amypo.in>
2025-12-15 10:42:20 +0000

    User commit

--- src/main/java/com/example/demo/entity/VerificationRule.java
+++ src/main/java/com/example/demo/entity/VerificationRule.java
@@ -5,5 +5,68 @@ public class VerificationRule {
     private String ruleCode;
     private String description;
     private String appliesToType;
+    private String validationExpression;
+    private boolean active;
+
+    public VerificationRule(){}
+
+    public VerificationRule(String ruleCode, String description, String appliesToType, String validationExpression,
+            boolean active) {
+        this.ruleCode = ruleCode;
+        this.description = description;
+        this.appliesToType = appliesToType;
+        this.validationExpression = validationExpression;
+        this.active = active;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getRuleCode() {
+        return ruleCode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getAppliesToType() {
+        return appliesToType;
+    }
+
+    public String getValidationExpression() {
+        return validationExpression;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setRuleCode(String ruleCode) {
+        this.ruleCode = ruleCode;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setAppliesToType(String appliesToType) {
+        this.appliesToType = appliesToType;
+    }
+
+    public void setValidationExpression(String validationExpression) {
+        this.validationExpression = validationExpression;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    
 
 }

317dce675ebc49f5494e21aac0a6d1f8d8f6cc41
bala-amypo <bala@amypo.in>
2025-12-18 05:25:03 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,85 +1,42 @@
 package com.example.demo.entity;
 
-import java.time.LocalDateTime;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
-@Table(name = "users")
+@Table(name="users")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
+
     private String fullName;
+
     @Column(unique = true)
     private String email;
+
     private String password;
-    private String role="VIEWER";
-    private LocalDateTime createdAt;
 
-    public User() {}
+    private String role;
 
-    public User(LocalDateTime createdAt, String email, String fullName, String password, String role) {
-        this.createdAt = createdAt;
-        this.email = email;
+    public User(){}
+
+    public User(String fullName, String email, String password, String role){
         this.fullName = fullName;
+        this.email = email;
         this.password = password;
         this.role = role;
     }
 
-    public long getId() {
-        return id;
-    }
+    public Long getId(){ return id; }
+    public String getFullName(){ return fullName; }
+    public String getEmail(){ return email; }
+    public String getPassword(){ return password; }
+    public String getRole(){ return role; }
 
-    public String getFullName() {
-        return fullName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-
-    
+    public void setId(Long id){ this.id=id; }
+    public void setFullName(String fullName){ this.fullName=fullName; }
+    public void setEmail(String email){ this.email=email; }
+    public void setPassword(String password){ this.password=password; }
+    public void setRole(String role){ this.role=role; }
 }
--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,25 +1,33 @@
 package com.example.demo.service.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.User;
+import com.example.demo.exception.BadRequestException;
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
-public class UserServiceImpl implements UserService{
-    
-    @Autowired
-    UserRepository userRepository;
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository repo;
 
     @Override
-    public User registerUser(User user) {
-        return userRepository.save(user);
+    public User registerUser(User user){
+
+        if(repo.existsByEmail(user.getEmail()))
+            throw new BadRequestException("Duplicate email!");
+
+        return repo.save(user);
     }
 
     @Override
-    public User findByEmail(String email) {
-        return userRepository.findByEmail(email).orElse(null);
+    public User findByEmail(String email){
+        return repo.findByEmail(email)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
     }
 }

9e7507b3255ae8087c9d2dd0451d561cedbe9806
bala-amypo <bala@amypo.in>
2025-12-18 08:04:59 +0000

    User commit

--- src/main/java/com/example/demo/service/AuditTrailService.java
+++ src/main/java/com/example/demo/service/AuditTrailService.java
@@ -1,38 +1,14 @@
-package com.example.demo.service.impl;
+package com.example.demo.service;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import org.springframework.stereotype.Service;
-import lombok.RequiredArgsConstructor;
 
 import com.example.demo.entity.AuditTrailRecord;
-import com.example.demo.exception.ResourceNotFoundException;
-import com.example.demo.repository.AuditTrailRecordRepository;
-import com.example.demo.service.AuditTrailService;
 
-@Service
-@RequiredArgsConstructor
-public class AuditTrailServiceImpl implements AuditTrailService {
+public interface AuditTrailService {
 
-    private final AuditTrailRecordRepository repo;
+    AuditTrailRecord logEvent(AuditTrailRecord record);
 
-    @Override
-    public AuditTrailRecord logEvent(AuditTrailRecord record){
-        if(record.getLoggedAt() == null)
-            record.setLoggedAt(LocalDateTime.now());
-        return repo.save(record);
-    }
+    List<AuditTrailRecord> getLogsByCredential(Long credentialId);
 
-    @Override
-	public List<AuditTrailRecord> getLogsByCredential(Long credentialId) {
-		boolean exists = repo.existsById(credentialId);
-        if(!exists)
-            throw new ResourceNotFoundException("Credential ID not found");
-        return repo.findByCredentialId(credentialId);
-    }
-
-    @Override
-    public List<AuditTrailRecord> getAllLogs() {
-        return repo.findAll();
-    }
+    List<AuditTrailRecord> getAllLogs();
 }

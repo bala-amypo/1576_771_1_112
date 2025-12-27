b680864f67fe0d5712fea074fb5d14b9c66f1c06
bala-amypo <bala@amypo.in>
2025-12-27 01:13:08 +0000

    User commit

--- src/main/java/com/example/demo/dto/JwtResponse.java
+++ src/main/java/com/example/demo/dto/JwtResponse.java
@@ -1,38 +1,10 @@
 package com.example.demo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class JwtResponse {
-
     private String token;
-    private Long id;
-    private String email;
-    private String role;
-
-    // REQUIRED by existing tests (DO NOT REMOVE)
-    public JwtResponse(String token) {
-        this.token = token;
-    }
-
-    // REQUIRED by your AuthController
-    public JwtResponse(String token, Long id, String email, String role) {
-        this.token = token;
-        this.id = id;
-        this.email = email;
-        this.role = role;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getRole() {
-        return role;
-    }
 }
